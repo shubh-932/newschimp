9031d627efba2b945220dcd29e7766a12e85d57d
shubh932 <agrawalshubh932@gmail.com>
2025-03-12 18:01:29 +0530

    adds logo and corrects country dropdown

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react'
 import { NavLink } from 'react-router-dom'
 import { ThemeContext } from '../contexts/ThemeContext';
 import { LocaleContext } from '../contexts/LocaleContext';
+import Logo from './../assets/images/logo.jpeg';
 
 export default function Navbar() {
 
@@ -20,6 +21,7 @@ export default function Navbar() {
   return (
     <nav className={`navbar fixed-top navbar-expand-lg navbar-${darkMode?'dark':'light'} bg-${darkMode?'dark':'light'}`}>
       <div className="container-fluid">
+        <img src={Logo} alt="Logo" style={{width:"35px",height:"35px"}} />
         <span className="navbar-brand">NewsChimp</span>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
@@ -27,9 +29,9 @@ export default function Navbar() {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <span className="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                 Country
-              </a>
+              </span>
               <ul className="dropdown-menu">
                 <li><button className="dropdown-item" onClick={() => setLocale('in')}>India</button></li>
                 <li><button className="dropdown-item" onClick={() => setLocale('en')}>USA</button></li>
